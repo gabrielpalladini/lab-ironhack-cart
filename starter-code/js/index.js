123cf27292304e1e04f07ca07b843b5e38a6d38a
Gabriel Palladini <gmpalladini@gmail.com>
2020-06-09 12:51:35 +0200

    initialcommit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,32 @@ var $calc = document.getElementById('calc');
 
 function updateSubtotal($product) {
   // Iteration 1.1
+ 
+document.querySelector(".btn.btn-success").addEventListener("click", () => {
+  let unitPrice = document.querySelectorAll(".pu span");
+  let quantity = document.querySelectorAll(".qty input");
+  let subTotal = document.querySelectorAll(".subtot span");
+  for (let i = 0; i < unitPrice.length; i++) {
+    subTotal[i].innerText = Number(unitPrice[i].innerText) * quantity[i].value
+  }
+})
 }
 
 function calculateAll() {
-  // Iteration 1.2
+  
 }
 $calc.onclick = calculateAll;
+
+
+document.querySelector(".btn.btn-success").addEventListener("click", () => {
+  let unitPrice = document.querySelectorAll(".pu span");
+  let quantity = document.querySelectorAll(".qty input");
+  let subTotal = document.querySelectorAll(".subtot span");
+  let totalPrice = 0;
+  for (let i = 0; i < unitPrice.length; i++) {
+    subTotal[i].innerText = Number(unitPrice[i].innerText) * quantity[i].value
+    totalPrice += Number(unitPrice[i].innerText) * quantity[i].value
+  }
+  document.querySelector("h2 span").innerText = totalPrice 
+})
+
