db2b3bab53806baf8437a2b02917ac654106e635
Gabriel Palladini <gmpalladini@gmail.com>
2020-06-11 16:59:10 +0200

    Gabriel & Ieva WebDev Berlin jun 2020 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -32,3 +32,97 @@ document.querySelector(".btn.btn-success").addEventListener("click", () => {
   document.querySelector("h2 span").innerText = totalPrice 
 })
 
+function addProduct () {
+  document.getElementById("create").addEventListener("click", () => {
+    let productName = document.querySelector(".new input")[0].value;
+    let productPrice = document.querySelector(".new input")[1].value 
+  })
+  let newProduct = document.createElement("div");
+  newProduct.innerText = "This works";
+  let parentElement = document.querySelector("tbody");
+  parentElement.appendChild(newProduct)
+}
+
+document.getElementById("create").addEventListener("click", () => {
+  let productName = document.querySelector(".new input")[0];
+  let productPrice = document.querySelector(".new input")[1]; 
+
+ let newProduct = document.createElement("div");
+newProduct.innerHTML = `<p> ${productName} </p>`
+
+/* `<tr class="product">
+<td class="name">
+  <div>
+  <span>${productName.value}</span>
+  </div>
+</td>
+
+<td class="pu">
+  <div>
+  $<span>${productPrice.value}</span>
+  </div>
+</td>
+
+<td class="qty">
+  <div>
+    <label>
+    <input type="number" value="0" min="0">
+  </label>
+</div>
+</td>
+
+<td class="subtot">
+  <div>
+  $<span>0</span>
+  </div>
+</td>
+
+<td class="rm">
+  <div>
+  <button class="btn btn-delete">Delete</button>
+  </div>
+</td>
+
+</tr>`*/;
+
+let parentElement = document.querySelector("tbody");
+parentElement.appendChild(newProduct)
+
+})
+
+/* <div>
+      <tr class="product">
+        <td class="name">
+          <div>
+          <span>IronBubble-head</span>
+          </div>
+        </td>
+    
+        <td class="pu">
+          <div>
+          $<span>25.00</span>
+          </div>
+        </td>
+    
+        <td class="qty">
+          <div>
+            <label>
+            <input type="number" value="0" min="0">
+          </label>
+        </div>
+        </td>
+    
+        <td class="subtot">
+          <div>
+          $<span>0</span>
+          </div>
+        </td>
+    
+        <td class="rm">
+          <div>
+          <button class="btn btn-delete">Delete</button>
+          </div>
+        </td>
+        
+      </tr>
+      </div> */
